656c79e72ae22586a7e0c5c3e1d405b4210caf41
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-07-09 02:10:05 +0400

    Explore the Data Frame

--- Project 1 - Data Visualization with Matplotlib/main.py
+++ Project 1 - Data Visualization with Matplotlib/main.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 colnames = ['DATE', 'TAG', 'POSTS']
 df = pd.read_csv("sample_data/QueryResults.csv", names=colnames, header=0)
+
+# Show First Five Rows
+print(df.head())
+# Show Last Five Rows
+print(df.tail())
+# Show the Number of Rows & Columns
+print(df.shape)
+# Count the number of entries in each column
+print(df.count())
